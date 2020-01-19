4c2510d0bcd373c7176caa2ed6480fd96830ed32
tevfikkoyun <tevfikkoyun97@gmail.com>
2020-01-18 21:22:59 -0500

    first commit

---
+++ koyun_Pa01/src/koyun_Pa01/firstexp.java
@@ -0,0 +1,12 @@
+package koyun_Pa01;
+
+public class firstexp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello world !!");
+	}
+
+}
